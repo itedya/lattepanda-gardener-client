1d0fbab0dc1567dd62d4c3ae0a6925d0370f196e
itedya <kontakt@itedya.com>
2022-05-01 23:24:43 +0200

    Added styling to arduino configuration

--- src/components/ArduinoConfiguration.vue
+++ src/components/ArduinoConfiguration.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="p-3">
-    <table>
+  <div class="p-5">
+    <table class="rounded-xl bg-slate-800 w-full">
       <thead>
       <tr>
-        <th>Nazwa konfiguracji</th>
+        <th class="p-3 text-gray-200 text-left">Nazwa konfiguracji</th>
         <th></th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="configuration in configurations">
-        <td>{{ configuration.name }}</td>
-        <td>
-          <button class="px-3 py-2 "></button>
+      <tr v-for="config in configurations">
+        <td class="p-3 text-gray-200"></td>
+        <td class="p-3 text-gray-200 flex gap-2">
+          <button class="px-3 py-2 bg-red-500 rounded-xl">
+            <img :src="'./icons/trash.svg'" alt="Trash icon">
+          </button>
+
+          <button class="px-3 py-2 bg-orange-500 rounded-xl">
+            <img :src="'./icons/edit.svg'" alt="Edit icon">
+          </button>
         </td>
       </tr>
       </tbody>

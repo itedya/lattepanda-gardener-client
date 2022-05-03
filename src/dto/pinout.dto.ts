5bf77c0a517ce16dc17c94ea5b0439e8d342c42f
itedya <kontakt@itedya.com>
2022-05-03 17:42:28 +0200

    Added constructor method to Pinout Dto

--- src/dto/pinout.dto.ts
+++ src/dto/pinout.dto.ts
@@ -1,5 +1,9 @@
 export default class PinoutDto {
     constructor(data: Partial<PinoutDto>) {
+        if (data.id) this.id = data.id;
+        if (data.sensorPin) this.sensorPin = data.sensorPin;
+        if (data.valvePin) this.valvePin = data.valvePin;
+        if (data.arduinoConfigurationId) this.arduinoConfigurationId = data.arduinoConfigurationId;
     }
 
     id: number;

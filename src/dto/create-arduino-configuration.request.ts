e06d601b78fd1399771bab124977ca7c4048d57d
itedya <kontakt@itedya.com>
2022-05-03 17:43:35 +0200

    Added "pinouts" property to Create Arduino Configuration Request

--- src/dto/create-arduino-configuration.request.ts
+++ src/dto/create-arduino-configuration.request.ts
@@ -1,9 +1,13 @@
+import PinoutDto from "./pinout.dto";
+
 export default class CreateArduinoConfigurationRequest {
     constructor(data: Partial<CreateArduinoConfigurationRequest>) {
         if (data.name) this.name = data.name;
         if (data.serialport) this.serialport = data.serialport;
+        if (data.pinouts) this.pinouts = data.pinouts;
     }
 
     name: string;
     serialport: string;
+    pinouts: PinoutDto[];
 }

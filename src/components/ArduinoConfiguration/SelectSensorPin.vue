8bd33577f2e4f9dab331b2713f554adb58becf06
itedya <kontakt@itedya.com>
2022-05-03 17:48:48 +0200

    Added select sensor pin component

---
+++ src/components/ArduinoConfiguration/SelectSensorPin.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="input-container">
+    <label class="input-label">Sensor PIN</label>
+    <select v-model="value" class="input">
+      <option v-for="i in [...Array(25).keys()]" :value="i">D{{ i }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import {ref, watch} from "vue";
+
+export default {
+  props: ['modelValue'],
+
+  setup(props, {emit}) {
+    const value = ref();
+
+    watch(props, (val) => {
+      value.value = val.modelValue;
+    }, {deep: true});
+
+    watch(value, (val) => {
+      emit("update:modelValue", val);
+    })
+
+    return {value};
+  }
+}
+</script>
